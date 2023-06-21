dee6e772e93728e65b93c0433f39dc0bdc6dfa2a
parkpoomd <parkpoom.d@gmail.com>
2023-06-21 14:46:34 +0700

    creating & joining subreddits

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/Toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
@@ -27,13 +28,14 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        <Navbar />
-        {authModal}
+        <Providers>
+          <Navbar />
+          {authModal}
 
-        <div className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </div>
-
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+        </Providers>
         <Toaster />
       </body>
     </html>
