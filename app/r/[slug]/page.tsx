1bbe8611055fd244c166c72a7e909f43d88ac7a5
parkpoomd <parkpoom.d@gmail.com>
2023-06-24 22:05:15 +0700

    voting for posts

--- app/r/[slug]/page.tsx
+++ app/r/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import MiniCreatePost from "@/components/MiniCreatePost";
+import PostFeed from "@/components/PostFeed";
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -25,6 +26,9 @@ const page = async ({ params }: PageProps) => {
           comments: true,
           subreddit: true,
         },
+        orderBy: {
+          createdAt: "desc",
+        },
 
         take: INFINITE_SCROLLING_PAGINATION_RESULTS,
       },
@@ -40,7 +44,7 @@ const page = async ({ params }: PageProps) => {
       </h1>
 
       <MiniCreatePost session={session} />
-      {/* TODO: Show posts in user feed */}
+      <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
     </>
   );
 };
